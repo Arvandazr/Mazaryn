4a694a4ff32a780aca30238e82f9072d49912bb5
Arvand <azadvararvand@gmail.com>
2021-06-19 19:48:48 +0430

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -9,4 +9,10 @@ router.register('posts', PostViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("create_post/", create_post, name="create_post"),
+    path("all_posts/", all_posts, name="all_posts"),
+    path("user_posts/<str:username>", user_posts, name="user_posts"),
+    path("edit_post/", edit_post, name="edit_post"),
+    path("like_post/<int:post_id>", like_post, name="like_post"),
+    path("does_like_post/<int:post_id>", does_like_post, name='does_like_post'),
 ]
